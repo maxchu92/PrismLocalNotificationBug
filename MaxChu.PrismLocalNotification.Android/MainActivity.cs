f5e0c6777767fed750553d635bbcc5b1ed95fb84
Makkusu <maxchu92@hotmail.com>
2021-05-28 18:02:30 +0800

    Updated .gitignore and activities

--- MaxChu.PrismLocalNotification.Android/MainActivity.cs
+++ MaxChu.PrismLocalNotification.Android/MainActivity.cs
@@ -8,6 +8,7 @@ using Prism.Ioc;
 
 namespace MaxChu.PrismLocalNotification.Droid {
 	[Activity(Theme = "@style/MainTheme",
+			  LaunchMode = LaunchMode.SingleTask,
 			  ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
 		protected override void OnCreate(Bundle savedInstanceState) {
--- MaxChu.PrismLocalNotification.Android/SplashActivity.cs
+++ MaxChu.PrismLocalNotification.Android/SplashActivity.cs
@@ -1,19 +1,25 @@
 using Android.App;
 using Android.Content;
 using AndroidX.AppCompat.App;
+using Plugin.LocalNotification;
 
-namespace MaxChu.PrismLocalNotification.Droid
-{
-    [Activity(Theme = "@style/MainTheme.Splash",
-              MainLauncher = true,
-              NoHistory = true)]
-    public class SplashActivity : AppCompatActivity
-    {
-        // Launches the startup task
-        protected override void OnResume()
-        {
-            base.OnResume();
-            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
-        }
-    }
+namespace MaxChu.PrismLocalNotification.Droid {
+	[Activity(Theme = "@style/MainTheme.Splash",
+			  MainLauncher = true,
+			  NoHistory = true)]
+	public class SplashActivity : AppCompatActivity {
+		// Launches the startup task
+		protected override void OnResume() {
+			base.OnResume();
+			var data = Intent.GetStringExtra(NotificationCenter.ExtraReturnDataAndroid);
+			var mainIntent = new Intent(Application.Context, typeof(MainActivity));
+
+			mainIntent.SetFlags(ActivityFlags.SingleTop);
+			if (!string.IsNullOrWhiteSpace(data)) {
+				mainIntent.PutExtra(NotificationCenter.ExtraReturnDataAndroid, data);
+			}
+
+			StartActivity(mainIntent);
+		}
+	}
 }
